90252116efc0eb68dac7c47a5a7ccd2f6148e87f
goodhumored <goodhumored@vk.com>
2023-05-22 19:55:07 +0300

    auth implemented

--- src/extension.ts
+++ src/extension.ts
@@ -5,9 +5,12 @@ export function activate(context: vscode.ExtensionContext) {
   
 	let disposable = vscode.commands.registerCommand('openproject.auth', () => {
 		vscode.window.showInputBox({ignoreFocusOut: true, password: true, prompt: "API token", title: "OpenProject API token"}).then(token => {
-      if (token) {vscode.window.showInformationMessage(`Hello, ${token}!`);}
+      if (!token) {return;}
+      vscode.window.showInformationMessage(`Hello, ${token}!`);
+      vscode.commands.executeCommand("setContext", "openproject.token", token);
     });
 	});
+  
 	context.subscriptions.push(disposable);
 }
 export function deactivate() {}
